92bd5bcab5eb1702ed0a778bd80107023a764df0
Sedona Thomas <sntsregister@gmail.com>
2021-10-13 12:11:29 -0400

    Create boing.js

---
+++ boing.js
@@ -0,0 +1,20 @@
+/*
+ * Sedona Thomas snt2127
+ * boing.js:
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) 
+{
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    
+    const playButton = document.getElementById("play");
+    playButton.addEventListener('click', play, false);
+
+    function play(event) 
+    {
+        
+        
+        
+    }
+    
+}, false);
